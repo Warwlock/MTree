9cc4dc6683d21255b331ac719c18277334218b11
WARWLOCK-PC\Warwlock <serhanbugrahan735@hotmail.com>
2026-03-18 11:25:11 +0300

    Add "MtreeShader" namespace to leaf and brak shader editors.

--- Editor/Shaders/BarkEditor.cs
+++ Editor/Shaders/BarkEditor.cs
@@ -4,111 +4,114 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class BarkEditor : ShaderGUI
+namespace MtreeShader
 {
-    enum DetailSettings
+    public class BarkEditor : ShaderGUI
     {
-        On,
-        Off
-    }
-
-    enum OcclusionSource
-    {
-        Color,
-        ShaderAmbient
-    }
-
-    DetailSettings dSets;
-    OcclusionSource occlSrc;
-
-    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
-    {
-        Material targetMat = materialEditor.target as Material;
-        //materialEditor.PropertiesDefaultGUI(properties);
-
-        //base.OnGUI(materialEditor, properties);
-        for (int i = 0; i < properties.Length; i++)
+        enum DetailSettings
         {
-            var property = properties[i];
+            On,
+            Off
+        }
 
-            DrawHeaders(i);
-            if ((property.propertyFlags & ShaderPropertyFlags.HideInInspector) == 0)
+        enum OcclusionSource
+        {
+            Color,
+            ShaderAmbient
+        }
+
+        DetailSettings dSets;
+        OcclusionSource occlSrc;
+
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+            Material targetMat = materialEditor.target as Material;
+            //materialEditor.PropertiesDefaultGUI(properties);
+
+            //base.OnGUI(materialEditor, properties);
+            for (int i = 0; i < properties.Length; i++)
             {
-                CheckProperty(materialEditor, property);
+                var property = properties[i];
+
+                DrawHeaders(i);
+                if ((property.propertyFlags & ShaderPropertyFlags.HideInInspector) == 0)
+                {
+                    CheckProperty(materialEditor, property);
+                }
             }
-        }
 
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        if (SupportedRenderingFeatures.active.editableMaterialRenderQueue)
-        {
-            materialEditor.RenderQueueField();
-        }
-        materialEditor.EnableInstancingField();
-        materialEditor.DoubleSidedGIField();
-    }
-
-    void CheckProperty(MaterialEditor materialEditor, MaterialProperty property)
-    {
-        if (property.name == "_CullMode")
-        {
-            //mode = (CullMode)property.floatValue;
-            //mode = (CullMode)DrawEnum(mode, property.displayName);
-            //property.floatValue = (int)mode;
-            return;
-        }
-        if (property.name == "_BaseDetail")
-        {
-            dSets = (DetailSettings)property.floatValue;
-            dSets = (DetailSettings)DrawEnum(dSets, property.displayName);
-            property.floatValue = (int)dSets;
-            return;
-        }
-        if (property.name == "_OcclusionSource")
-        {
-            occlSrc = (OcclusionSource)property.floatValue;
-            occlSrc = (OcclusionSource)DrawEnum(occlSrc, property.displayName);
-            property.floatValue = (int)occlSrc;
-            return;
-        }
-        materialEditor.ShaderProperty(property, property.displayName);
-    }
-
-    System.Enum DrawEnum(System.Enum mode, string label)
-    {
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField(label);
-        mode = EditorGUILayout.EnumPopup(mode);
-        EditorGUILayout.EndHorizontal();
-        return mode;
-    }
-
-    void DrawHeaders(int index)
-    {
-        GUIStyle headerStyle = new GUIStyle(EditorStyles.boldLabel);
-        headerStyle.fontSize = 14;
-
-        if (index == 0)
-        {
-            EditorGUILayout.LabelField("General Settings", headerStyle);
-        }
-
-        if (index == 5)
-        {
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Detail Settings", headerStyle);
+            EditorGUILayout.Space();
+            if (SupportedRenderingFeatures.active.editableMaterialRenderQueue)
+            {
+                materialEditor.RenderQueueField();
+            }
+            materialEditor.EnableInstancingField();
+            materialEditor.DoubleSidedGIField();
         }
 
-        if (index == 12)
+        void CheckProperty(MaterialEditor materialEditor, MaterialProperty property)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Other Settings", headerStyle);
+            if (property.name == "_CullMode")
+            {
+                //mode = (CullMode)property.floatValue;
+                //mode = (CullMode)DrawEnum(mode, property.displayName);
+                //property.floatValue = (int)mode;
+                return;
+            }
+            if (property.name == "_BaseDetail")
+            {
+                dSets = (DetailSettings)property.floatValue;
+                dSets = (DetailSettings)DrawEnum(dSets, property.displayName);
+                property.floatValue = (int)dSets;
+                return;
+            }
+            if (property.name == "_OcclusionSource")
+            {
+                occlSrc = (OcclusionSource)property.floatValue;
+                occlSrc = (OcclusionSource)DrawEnum(occlSrc, property.displayName);
+                property.floatValue = (int)occlSrc;
+                return;
+            }
+            materialEditor.ShaderProperty(property, property.displayName);
         }
 
-        if (index == 16)
+        System.Enum DrawEnum(System.Enum mode, string label)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Wind Settings", headerStyle);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label);
+            mode = EditorGUILayout.EnumPopup(mode);
+            EditorGUILayout.EndHorizontal();
+            return mode;
+        }
+
+        void DrawHeaders(int index)
+        {
+            GUIStyle headerStyle = new GUIStyle(EditorStyles.boldLabel);
+            headerStyle.fontSize = 14;
+
+            if (index == 0)
+            {
+                EditorGUILayout.LabelField("General Settings", headerStyle);
+            }
+
+            if (index == 5)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Detail Settings", headerStyle);
+            }
+
+            if (index == 12)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Other Settings", headerStyle);
+            }
+
+            if (index == 16)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Wind Settings", headerStyle);
+            }
         }
     }
 }
--- Editor/Shaders/LeafEditor.cs
+++ Editor/Shaders/LeafEditor.cs
@@ -4,184 +4,187 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.Rendering;
 
-public class LeafEditor : ShaderGUI
+namespace MtreeShader
 {
-    enum CullMode
+    public class LeafEditor : ShaderGUI
     {
-        Off,
-        Front,
-        Back
-    }
-
-    enum DoubleSidedNormal
-    {
-        Flip,
-        Mirror,
-        None
-    }
-
-    enum ColorShifting
-    {
-        On,
-        Off
-    }
-
-    enum TranslucencyEnum
-    {
-        Off,
-        On
-    }
-
-    enum ColorSource
-    {
-        Global,
-        Custom
-    }
-
-    enum WindModeLeaves
-    {
-        Leaves,
-        Palm,
-        Grass,
-        Off
-    }
-
-    enum OcclusionSource
-    {
-        Color,
-        ShaderAmbient
-    }
-
-    CullMode mode;
-    DoubleSidedNormal dsNorm;
-    ColorShifting colShift;
-    TranslucencyEnum transEnum;
-    ColorSource colSource;
-    WindModeLeaves windMode;
-    OcclusionSource occlSrc;
-
-    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
-    {
-        Material targetMat = materialEditor.target as Material;
-        //materialEditor.PropertiesDefaultGUI(properties);
-
-        //base.OnGUI(materialEditor, properties);
-        for (int i = 0; i < properties.Length; i++)
+        enum CullMode
         {
-            var property = properties[i];
+            Off,
+            Front,
+            Back
+        }
 
-            DrawHeaders(i);
-            if ((property.propertyFlags & ShaderPropertyFlags.HideInInspector) == 0)
+        enum DoubleSidedNormal
+        {
+            Flip,
+            Mirror,
+            None
+        }
+
+        enum ColorShifting
+        {
+            On,
+            Off
+        }
+
+        enum TranslucencyEnum
+        {
+            Off,
+            On
+        }
+
+        enum ColorSource
+        {
+            Global,
+            Custom
+        }
+
+        enum WindModeLeaves
+        {
+            Leaves,
+            Palm,
+            Grass,
+            Off
+        }
+
+        enum OcclusionSource
+        {
+            Color,
+            ShaderAmbient
+        }
+
+        CullMode mode;
+        DoubleSidedNormal dsNorm;
+        ColorShifting colShift;
+        TranslucencyEnum transEnum;
+        ColorSource colSource;
+        WindModeLeaves windMode;
+        OcclusionSource occlSrc;
+
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+            Material targetMat = materialEditor.target as Material;
+            //materialEditor.PropertiesDefaultGUI(properties);
+
+            //base.OnGUI(materialEditor, properties);
+            for (int i = 0; i < properties.Length; i++)
             {
-                CheckProperty(materialEditor, property);
+                var property = properties[i];
+
+                DrawHeaders(i);
+                if ((property.propertyFlags & ShaderPropertyFlags.HideInInspector) == 0)
+                {
+                    CheckProperty(materialEditor, property);
+                }
             }
-        }
 
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        if (SupportedRenderingFeatures.active.editableMaterialRenderQueue)
-        {
-            materialEditor.RenderQueueField();
-        }
-        materialEditor.EnableInstancingField();
-        materialEditor.DoubleSidedGIField();
-    }
-
-    void CheckProperty(MaterialEditor materialEditor, MaterialProperty property)
-    {
-        if(property.name == "_CullMode")
-        {
-            mode = (CullMode)property.floatValue;
-            mode = (CullMode)DrawEnum(mode, property.displayName);
-            property.floatValue = (int)mode;
-            return;
-        }
-        if (property.name == "_DoubleSidedNormalMode")
-        {
-            dsNorm = (DoubleSidedNormal)property.floatValue;
-            dsNorm = (DoubleSidedNormal)DrawEnum(dsNorm, property.displayName);
-            property.floatValue = (int)dsNorm;
-            return;
-        }
-        if (property.name == "_ColorShifting")
-        {
-            colShift = (ColorShifting)property.floatValue;
-            colShift = (ColorShifting)DrawEnum(colShift, property.displayName);
-            property.floatValue = (int)colShift;
-            return;
-        }
-        if (property.name == "_TranslucencyEnum")
-        {
-            transEnum = (TranslucencyEnum)property.floatValue;
-            transEnum = (TranslucencyEnum)DrawEnum(transEnum, property.displayName);
-            property.floatValue = (int)transEnum;
-            return;
-        }
-        if (property.name == "_ColorSource")
-        {
-            colSource = (ColorSource)property.floatValue;
-            colSource = (ColorSource)DrawEnum(colSource, property.displayName);
-            property.floatValue = (int)colSource;
-            return;
-        }
-        if (property.name == "_WindModeLeaves")
-        {
-            windMode = (WindModeLeaves)property.floatValue;
-            windMode = (WindModeLeaves)DrawEnum(windMode, property.displayName);
-            property.floatValue = (int)windMode;
-            return;
-        }
-        if (property.name == "_OcclusionSource")
-        {
-            occlSrc = (OcclusionSource)property.floatValue;
-            occlSrc = (OcclusionSource)DrawEnum(occlSrc, property.displayName);
-            property.floatValue = (int)occlSrc;
-            return;
-        }
-        materialEditor.ShaderProperty(property, property.displayName);
-    }
-
-    System.Enum DrawEnum(System.Enum mode, string label)
-    {
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField(label);
-        mode = EditorGUILayout.EnumPopup(mode);
-        EditorGUILayout.EndHorizontal();
-        return mode;
-    }
-
-    void DrawHeaders(int index)
-    {
-        GUIStyle headerStyle = new GUIStyle(EditorStyles.boldLabel);
-        headerStyle.fontSize = 14;
-
-        if (index == 0)
-        {
-            EditorGUILayout.LabelField("General Settings", headerStyle);
-        }
-
-        if (index == 7)
-        {
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Color Settings", headerStyle);
+            EditorGUILayout.Space();
+            if (SupportedRenderingFeatures.active.editableMaterialRenderQueue)
+            {
+                materialEditor.RenderQueueField();
+            }
+            materialEditor.EnableInstancingField();
+            materialEditor.DoubleSidedGIField();
         }
 
-        if (index == 12)
+        void CheckProperty(MaterialEditor materialEditor, MaterialProperty property)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Translucency Settings", headerStyle);
+            if(property.name == "_CullMode")
+            {
+                mode = (CullMode)property.floatValue;
+                mode = (CullMode)DrawEnum(mode, property.displayName);
+                property.floatValue = (int)mode;
+                return;
+            }
+            if (property.name == "_DoubleSidedNormalMode")
+            {
+                dsNorm = (DoubleSidedNormal)property.floatValue;
+                dsNorm = (DoubleSidedNormal)DrawEnum(dsNorm, property.displayName);
+                property.floatValue = (int)dsNorm;
+                return;
+            }
+            if (property.name == "_ColorShifting")
+            {
+                colShift = (ColorShifting)property.floatValue;
+                colShift = (ColorShifting)DrawEnum(colShift, property.displayName);
+                property.floatValue = (int)colShift;
+                return;
+            }
+            if (property.name == "_TranslucencyEnum")
+            {
+                transEnum = (TranslucencyEnum)property.floatValue;
+                transEnum = (TranslucencyEnum)DrawEnum(transEnum, property.displayName);
+                property.floatValue = (int)transEnum;
+                return;
+            }
+            if (property.name == "_ColorSource")
+            {
+                colSource = (ColorSource)property.floatValue;
+                colSource = (ColorSource)DrawEnum(colSource, property.displayName);
+                property.floatValue = (int)colSource;
+                return;
+            }
+            if (property.name == "_WindModeLeaves")
+            {
+                windMode = (WindModeLeaves)property.floatValue;
+                windMode = (WindModeLeaves)DrawEnum(windMode, property.displayName);
+                property.floatValue = (int)windMode;
+                return;
+            }
+            if (property.name == "_OcclusionSource")
+            {
+                occlSrc = (OcclusionSource)property.floatValue;
+                occlSrc = (OcclusionSource)DrawEnum(occlSrc, property.displayName);
+                property.floatValue = (int)occlSrc;
+                return;
+            }
+            materialEditor.ShaderProperty(property, property.displayName);
         }
 
-        if (index == 19)
+        System.Enum DrawEnum(System.Enum mode, string label)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Other Settings", headerStyle);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label);
+            mode = EditorGUILayout.EnumPopup(mode);
+            EditorGUILayout.EndHorizontal();
+            return mode;
         }
 
-        if (index == 23)
+        void DrawHeaders(int index)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Wind Settings", headerStyle);
+            GUIStyle headerStyle = new GUIStyle(EditorStyles.boldLabel);
+            headerStyle.fontSize = 14;
+
+            if (index == 0)
+            {
+                EditorGUILayout.LabelField("General Settings", headerStyle);
+            }
+
+            if (index == 7)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Color Settings", headerStyle);
+            }
+
+            if (index == 12)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Translucency Settings", headerStyle);
+            }
+
+            if (index == 19)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Other Settings", headerStyle);
+            }
+
+            if (index == 23)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Wind Settings", headerStyle);
+            }
         }
     }
 }
